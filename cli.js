5c71710789c8b693914968c2e692112e5cbd9ae2
David <uzondudavid2004@gmail.com>
2024-01-15 13:12:28 +0100

    fixed some bugs

--- cli.js
+++ cli.js
@@ -4,17 +4,19 @@ import { program } from "commander";
 import { init } from "./index.js";
 import clipboard from "clipboardy";
 
-program.option("-c", "this optional flag tells Yak to add the results to your clipboard.").option("--link <link>", "The link you want to parse.").action(async ({ c, link })=>{
+program
+  .option("-c", "this optional flag tells Yak to add the results to your clipboard.")
+  .option("--link <link>", "The link you want to parse.")
+  .action(async ({ c, link }) => {
     console.log(`Parsing: ${link}`);
     let response = await init(link);
-    // console.log(Promise.all(response)
+    console.log(response);
     if (response) {
-        console.log(response);
-        if (c) {
-            clipboard.writeSync(response);
-        }
+      if (c) {
+        clipboard.writeSync(`${response}`);
+        console.log("Hi");
+      }
     }
-});
-
+  });
 
 program.parse();
